8b893af4856affe20fd712c0a3e7d1e9e0f1b0c4
Subramanian Ramachandran <sramachandran@riverbed.com>
2017-06-21 12:15:00 -0700

    sudoku

--- g4g/add_bin.py
+++ g4g/add_bin.py
@@ -1,6 +1,13 @@
 #!/bin/python
+'''
+Given two strings representing binary numbers return the sum of these two
+numbers in binary format as a string
+
+Input May have leading zeros
+Output should not have leading zeros
+
+'''
 def addBinary(A, B):
-    import pdb; pdb.set_trace()
     m = len(A)
     n = len(B)
     if m < n:
@@ -9,11 +16,11 @@ def addBinary(A, B):
     elif m > n:
         B = '0'*(m-n) + B
         n = m
-    print "A={}, B= {}\n".format(A, B)
+    #print "A={}, B= {}\n".format(A, B)
     R = ''
     c = 0
     for i in range(m-1, -1, -1):
-        print 'i={} A[i]={} B[i]={} c={} R={}'.format(i, A[i], B[i], c, R)
+        #print 'i={} A[i]={} B[i]={} c={} R={}'.format(i, A[i], B[i], c, R)
         x = (int(A[i]) ^ int(B[i])) ^ c
         R = str(x) + R
         c = (int(A[i]) & int(B[i])) | ( (int(A[i]) | int(B[i])) & c)
